922c249a1fc7e4d1db965ab65e7c866723afaa73
eduams1 <eduardo4ms@gmail.com>
2023-05-29 00:36:28 -0300

    Galeria e animações

---
+++ js/main.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {gallery()};
+
+var elements = document.getElementsByClassName("gallery-pic");
+
+function gallery() {
+  if (document.body.scrollTop > (window.innerHeight * 0.3) || document.documentElement.scrollTop > (window.innerHeight * 0.3)) {
+    for(i=0;i<=7;i++){
+        elements[i].style.opacity = "1";
+    }
+  }
+}
+
+function galleryonhover(pic){
+  for(i = 0; i <=7; i++){
+    elements[i].style.width = '100%';
+    elements[i].style.height = '100%';
+    elements[i].style.margin = '0%';
+    elements[i].style.marginLeft = '0%';
+    elements[i].style.zIndex = '0';  
+  }
+  elements[pic].style.margin = '-20%';
+  elements[pic].style.marginLeft = '-10%';
+  elements[pic].style.zIndex = '99';
+  elements[pic].style.width = '120%';
+  elements[pic].style.height = '120%';
+}
